7bdf39c522e139b89b4ceda14a83e773fb1236b7
physwizz <pollockg86@gmail.com>
2023-10-27 10:57:35 +1100

    update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 
-export PLATFORM_VERSION=12
-export ANDROID_MAJOR_VERSION=s
+export PLATFORM_VERSION=13
+export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
-make ARCH=arm64 exynos850-a12snsxx_defconfig
+make ARCH=arm64 physwizz_defconfig
 make ARCH=arm64 -j64
+
+
